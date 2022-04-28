acdfcf129562ff0fa9163f2ca30bda816586a7e4
David McAfee <mcafd@amazon.com>
2022-04-28 15:45:46 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # appsync-sdk-custom-domain
+
+## Domain Name
+
+api.davidmcafee.com
+
+## AppSync Domain Name
+
+d1qeotpk9797l5.cloudfront.net
+
+## AppSync GraphQL Endpoint
+
+https://api.davidmcafee.com/graphql
+
+## AppSync Real-time Endpoint
+
+wss://api.davidmcafee.com/graphql/realtime
+
+curl -s -XPOST https://api.davidmcafee.com/graphql -H "Content-Type:application/graphql" -H "x-api-key:da2-dnpf6tuipzfmniwgrzpfxsmasa" -d '{"query": "mutation CreateTodo ($name: String!) {createTodo(description: $name) {id name}}","variables":"{\"name\":\"Birthday\"}"}'
--- react-apollo-V3-custom-domain/src/WithProvider.js
+++ react-apollo-V3-custom-domain/src/WithProvider.js
@@ -1,39 +1,39 @@
-import { createAuthLink } from 'aws-appsync-auth-link';
-import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
+import { createAuthLink } from "aws-appsync-auth-link";
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link";
 
 import {
-	ApolloLink,
-	ApolloClient,
-	ApolloProvider,
-	InMemoryCache,
-} from '@apollo/client';
+  ApolloLink,
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
 
-import AppSyncConfig from './aws-exports';
+import AppSyncConfig from "./aws-exports";
 
-import App from './App';
+import App from "./App";
 
 const url = AppSyncConfig.aws_appsync_graphqlEndpoint;
 const region = AppSyncConfig.aws_appsync_region;
 const auth = {
-	type: 'API_KEY',
-	apiKey: 'da2-u2d6ol5adbaopmdcwlhguhpvji',
+  type: AppSyncConfig.aws_appsync_authenticationType,
+  apiKey: AppSyncConfig.aws_appsync_apiKey,
 };
 
 const link = ApolloLink.from([
-	createAuthLink({ url, region, auth }),
-	// https://github.com/awslabs/aws-mobile-appsync-sdk-js/issues/619
-	createSubscriptionHandshakeLink({ url, region, auth }),
+  createAuthLink({ url, region, auth }),
+  // https://github.com/awslabs/aws-mobile-appsync-sdk-js/issues/619
+  createSubscriptionHandshakeLink({ url, region, auth }),
 ]);
 
 const client = new ApolloClient({
-	link,
-	cache: new InMemoryCache(),
+  link,
+  cache: new InMemoryCache(),
 });
 
 const WithProvider = () => (
-	<ApolloProvider client={client}>
-		<App />
-	</ApolloProvider>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
 
 export default WithProvider;
