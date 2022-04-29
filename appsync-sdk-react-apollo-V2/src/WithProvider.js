d8f25e7ec90948269dd6fdd508f83599d62fbf44
David McAfee <mcafd@amazon.com>
2022-04-28 19:35:57 -0700

    update apollo v2 auth testing

--- appsync-sdk-react-apollo-V2/src/WithProvider.js
+++ appsync-sdk-react-apollo-V2/src/WithProvider.js
@@ -1,25 +1,49 @@
-import AWSAppSyncClient from 'aws-appsync';
-import AppSyncConfig from './aws-exports';
-import { ApolloProvider } from 'react-apollo';
-import { Rehydrated } from 'aws-appsync-react';
+import AWSAppSyncClient from "aws-appsync";
+import AppSyncConfig from "./aws-exports";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import App from "./App";
 
-import App from './App';
+const url = `https://api2.davidmcafee.com/graphql`;
 
+// API KEY
 const client = new AWSAppSyncClient({
-	url: AppSyncConfig.aws_appsync_graphqlEndpoint,
-	region: AppSyncConfig.aws_appsync_region,
-	auth: {
-		type: AppSyncConfig.aws_appsync_authenticationType,
-		apiKey: AppSyncConfig.aws_appsync_apiKey,
-	},
+  url,
+  region: AppSyncConfig.aws_appsync_region,
+  auth: {
+    type: AppSyncConfig.aws_appsync_authenticationType,
+    apiKey: AppSyncConfig.aws_appsync_apiKey,
+  },
 });
 
+// COGNITO
+// const client = new AWSAppSyncClient({
+//   url,
+//   region: AppSyncConfig.aws_appsync_region,
+//   auth: {
+//     type: AppSyncConfig.aws_appsync_authenticationType,
+//     jwtToken: async () =>
+//       (await Auth.currentSession()).getIdToken().getJwtToken(),
+//   },
+// });
+
+// IAM
+// const client = new AWSAppSyncClient({
+//   url,
+//   region: AppSyncConfig.aws_appsync_region,
+//   auth: {
+//     type: AppSyncConfig.aws_appsync_authenticationType,
+//     credentials: () => Auth.currentCredentials(),
+//   },
+// });
+
 const WithProvider = () => (
-	<ApolloProvider client={client}>
-		<Rehydrated>
-			<App />
-		</Rehydrated>
-	</ApolloProvider>
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
 );
 
-export default WithProvider;
+export default withAuthenticator(WithProvider);
